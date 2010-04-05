fa26c266613c5f2986d2e0470ae26e18b2c5e329
jerome.lacoste <jerome.lacoste@gmail.com>
2010-04-05 16:23:49 -0600

    Remove redundant code

--- standalone_py/talkmoreCLI.py
+++ standalone_py/talkmoreCLI.py
@@ -34,27 +34,21 @@ def main():
 
     print "Balance: " + str(tm.balance) + " NOK"
 
-    if (raw_input("Do you want to send an SMS (y/n)? ")) == "y":
-        while 1: # Looping in case the user wants to send SMSs several times
-            sendMsg = True # Tell us if we can send the message or not
-            theNumbers = raw_input("Please enter the numbers. comma separated: ")
-            theMsg = raw_input("Please write your message: ")
-            telNbs = theNumbers.split(",") # splits the list of CSV into an array
-            print "You are going to send an SMS to", telNbs, "with the message :\n", theMsg
-            if (len(theMsg) > 160): # Checkking if the message is longer than one SMS
-                print "Your message is",len(theMsg),"chars long which is more than one SMS (",len(theMsg)/160+1,"SMSs to be precise)"
-                if (raw_input("Do you really want to send it (y/n)? ")) != "y":
-                    print "Not sending the SMS..."
-                    sendMsg = False
-            if (sendMsg == True):
-                print "Sending the SMS(s)..."
-                tm.send_sms(telNbs, theMsg)
-                print "SMS(s) sent!"
-            if (raw_input("One more time (y/n)? ")) == "y":
-                continue
-            else:
-                break
-
+    while raw_input("Do you want to send an SMS (y/n)? ") == "y": # Looping in case the user wants to send SMSs several times
+        sendMsg = True # Tell us if we can send the message or not
+        theNumbers = raw_input("Please enter the numbers. comma separated: ")
+        theMsg = raw_input("Please write your message: ")
+        telNbs = theNumbers.split(",") # splits the list of CSV into an array
+        print "You are going to send an SMS to", telNbs, "with the message :\n", theMsg
+        if (len(theMsg) > 160): # Checkking if the message is longer than one SMS
+            print "Your message is",len(theMsg),"chars long which is more than one SMS (",len(theMsg)/160+1,"SMSs to be precise)"
+            if (raw_input("Do you really want to send it (y/n)? ")) != "y":
+                print "Not sending the SMS..."
+                sendMsg = False
+        if (sendMsg == True):
+            print "Sending the SMS(s)..."
+#            tm.send_sms(telNbs, theMsg)
+            print "SMS(s) sent!"
 
     tm.logout()
     if not tm.is_logged_in():
