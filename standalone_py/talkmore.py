50cb7e46fe62e266a1f1526a9c8e71208417065f
jerome.lacoste <jerome.lacoste@gmail.com>
2010-04-04 18:49:29 -0600

    Extract API to reuse it

--- standalone_py/talkmore.py
+++ standalone_py/talkmore.py
@@ -1,115 +1,11 @@
 #!/usr/bin/python
 
-# requires python-wxgtk2.8 on Debian/Ubuntu
-import httplib2
-from urllib import urlencode
-
 # for some reason, some users have issues with from xx import yy style
 import wx
 import wx.lib.delayedresult
 print "Usign wx " + wx.VERSION_STRING
 
-import os
-import base64
-import re
-#import pickle
-
-class MyException(Exception):
-  def __init__(self, value):
-    self.value = value
-  def __str__(self):
-    return repr(self.value)
-
-class Talkmore:
-	user=None
-	cookie=None
-	balance=None
-
-	def login(self, login, pwd):
-		data = {'username':login, 'password':pwd}
-		headers = {'Content-type': 'application/x-www-form-urlencoded'}
-		h = httplib2.Http(".cache")
-		response, content = h.request('https://www.talkmore.no/talkmore3/servlet/Login', 'POST', headers=headers, body=urlencode(data))
-		self.cookie = response['set-cookie']
-		
-		self.update_balance()
-		self.user=login
-		# FIXME check logged in (e.g. find phone number on page + response code)
-		# check response - location +uid
-		# parse ringesaldo
-		
-	def update_balance(self):
-		if not self.is_logged_in():
-			raise MyException("Must be logged int to parse balance")
-		self.balance = self.parse_balance_page('https://www.talkmore.no/talkmore3/servlet/SubscriptionUsage')
-		
-	def parse_balance_page(self, url):
-		h = httplib2.Http(".cache")
-		headers = {'Cookie' : self.cookie }
-		response, content = h.request(url, 'GET', headers=headers)
-		m = re.search('.*Ringesaldo:.* &nbsp;(.*)  kroner.*', content)
-		if response.status !=  200:
-			raise MyException("couldn't open " + url + " status " + str(response.status))
-		return m.group(1)
-
-	def is_logged_in(self):
-		return self.cookie is not None
-		
-	def get_balance(self):
-		self.update_balance()
-		return self.balance
-		
-	def send_sms(self, to_numbers, message):
-		'''We support sending messages to multiple numbers. The first parameter is an array of Strings representing norwegian numbers.
-		   The second parameter should be the message, max 1600 characters (dixit talkmore web interface, I haven't tried it'''
-		if not self.is_logged_in():
-			raise MyException("Must be logged int to send SMS")
-
-	    # FIXME check numbers (must be norwegian)
-		number_list = ';'.join(to_numbers)
-		data = {'list':number_list, 'message1':message}
-		headers = {'Content-type': 'application/x-www-form-urlencoded', 'Cookie' : self.cookie}
-		h = httplib2.Http(".cache")
-		response, content = h.request('https://www.talkmore.no/talkmore3/servlet/SendSmsFromSelfcare', 'POST', headers=headers, body=urlencode(data))
-		if response.status !=  200:
-			raise MyException("couldn't send SMS to " + number_list + ". status " + str(response.status))
-		m = re.search('.*Du har sendt +(.*) +SMS hittil i dag.*', content)
-#		print content
-		print "Sent SMS(es): " + m.group(1)
-		
-	def logout(self):
-		headers = {'Cookie' : self.cookie }
-		h = httplib2.Http(".cache")
-		response, content = h.request('https://www.talkmore.no/talkmore3/servlet/Logout', 'GET', headers=headers)
-		self.cookie = None
-		self.user = None
-
-def get_profile_dir():
-	profile_dir = os.path.expanduser("~/.talkmore/")
-	if not os.path.exists(profile_dir):
-		os.mkdir(profile_dir)
-		# FIXME error check
-	return profile_dir
-
-def get_credentials():
-	'Return a tuple login,password or None if no credentials saved'
-	profile_dir = get_profile_dir()
-	f = open(os.path.join(profile_dir, "credentials"), "r")
-	login = f.readline().rstrip('\n')
-	password = base64.b64decode(f.readline().rstrip('\n'))
-	return login, password
-#	res = pickle.load(f)
-#	f.close()
-#	return res[0], base64.b64decode(res[1])
-
-def save_credentials(login, password):
-	'Save credentials into home file'
-	profile_dir = get_profile_dir()
-	f = open(os.path.join(profile_dir, "credentials"), "w")
-	#pickle.dump([login, base64.b64encode(password)], f)
-	f.write(login + "\n")
-	f.write(base64.b64encode(password) + "\n")
-	f.close()
+from talkmoreapi import *
 
 class MyFrame(wx.Frame):		
 	tm = Talkmore()
